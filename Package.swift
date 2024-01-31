89e5f28097a11bd11af5eb05ba3ecf4de4796cad
Martin Wright <martin.wright@evergreenapps.co.uk>
2024-01-31 08:33:00 +0000

    Fix Starscream version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-      .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.0"))
+      .package(url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.4"))
     ],
     targets: [
         .target(
